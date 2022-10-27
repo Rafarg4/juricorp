13326fb61c7d48853a2896fb6f031cec175fe26d
jlboronowskidev <jlboronowskidev@gmail.com>
2022-10-27 17:48:01 -0300

    Modal para editar pago

--- resources/views/expedientes/pagos.blade.php
+++ resources/views/expedientes/pagos.blade.php
@@ -2,6 +2,7 @@
     <table class="table" id="example1">
         <thead>
         <tr>
+             <th style="display:none;">id</th>
             <th>Concepto</th>
         <th>Monto</th>
         <th>Fecha</th>
@@ -13,18 +14,20 @@
         <tbody>
         @foreach($pagoExpedientes as $pagoExpediente)
             <tr>
-                <td>{{ $pagoExpediente->concepto }}</td>
-            <td>{{number_format ($pagoExpediente->monto) }}</td>
-            <td>{{ $pagoExpediente->fecha }}</td>
-            <td>{{ $pagoExpediente->expediente->numero }}</td>
-            <td><img src="{{ asset('storage').'/'.$pagoExpediente->archivo}}" width="50" height="50"></td>
+                <td style="display:none;" class="id_pago">{{ $pagoExpediente->id_expediente }}</td>
+                <td class="concepto">{{ $pagoExpediente->concepto }}</td>
+            <td class="monto">{{number_format ($pagoExpediente->monto) }}</td>
+            <td class="fecha">{{ $pagoExpediente->fecha }}</td>
+            <td class="numero">{{ $pagoExpediente->expediente->numero }}</td>
+            <td class="img"><img src="{{ asset('storage').'/'.$pagoExpediente->archivo}}" width="50" height="50"></td>
                 <td width="120">
                     {!! Form::open(['route' => ['pagoExpedientes.destroy', $pagoExpediente->id], 'method' => 'delete']) !!}
                     <div class='btn-group'>
-                        <a href="{{ route('pagoExpedientes.show', [$pagoExpediente->id]) }}"
+                        <a href=""  
                            class='btn btn-default btn-xs'>
                             <i class="far fa-eye"></i>
                         </a>
+                        <button type="button" class="btn editarPagoA"><i class="far fa-eye"></i></button>
                         <a href="{{ route('pagoExpedientes.edit', [$pagoExpediente->id]) }}"
                            class='btn btn-default btn-xs'>
                             <i class="far fa-edit"></i>
@@ -48,3 +51,101 @@
   </tfoot>
     </table>
 </div>
+
+
+
+
+<div class="modal fade" id="editarPago" tabindex="-1" role="dialog" aria-labelledby="editarPagoLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="editarPagoLabel">Cargar un nuevo pago</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+   
+  <div class="content px-3">
+
+        @include('adminlte-templates::common.errors')
+
+        <div class="card">
+
+           <form>
+            
+
+            <div class="card-body">
+
+                <div class="row">
+                    @include('pago_expedientes.fields')
+                </div>
+
+            </div>
+
+            <div class="card-footer">
+              
+               
+            </div>
+
+           
+
+        </div>
+    </div>
+      <div class="modal-footer">
+        
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+          {!! Form::submit('Guardar', ['class' => 'btn btn-primary', 'id' => 'editarPagoBoton']) !!}
+           </form>
+      </div>
+    </div>
+  </div>
+</div>
+<script>
+    $('.btn.editarPagoA').click(function(){
+
+   var row = $(this).closest("tr");   // Find the row
+    var concepto = row.find(".concepto").text();// Find the text
+    var fecha = row.find(".fecha").text();
+    var monto = row.find(".monto").text();
+    var numero = row.find(".numero").text();
+    var id = row.find(".id_pago").text();
+    var archivo = row.find(".archivo").text();
+   
+
+     console.log(id);
+    $('#concepto').val(concepto);
+    $('#fecha').val(fecha);
+    $('#monto').val(monto);
+    $('#numero').val(numero);
+    $('#id_expediente').val(id);
+     $('#archivo').val(archivo);
+
+
+
+    $('#editarPago').modal('toggle');
+    });
+
+    
+    $('#editarPagoBoton').click(function(){
+            var id_expediente = $('#id_expediente').val();
+            var concepto_gasto = $('#concepto_gasto').val(); 
+            var monto_gasto = $('#monto_gasto').val();
+            var fecha_gasto = $('#fecha_gasto').val();
+            var archivo_gasto = $('#archivo_gasto').val();
+         
+     $.ajax({
+               type:'PATCH',
+               url:'/pagoExpedientes',
+               data:{  "_token": "{{ csrf_token() }}", concepto: concepto, monto: monto, fecha: fecha, id_expediente: id_expediente},
+               success:function(data) {
+                  $('#editarPago').modal('hide');
+               }
+            });
+
+            });   
+   
+    // Let's test it out
+
+
+   
+   </script>
--- resources/views/pago_expedientes/fields.blade.php
+++ resources/views/pago_expedientes/fields.blade.php
@@ -7,23 +7,23 @@
 <!-- Monto Field -->
 <div class="form-group col-sm-6">
     {!! Form::label('monto', 'Monto:') !!}
-    {!! Form::number('monto', null, ['class' => 'form-control']) !!}
+    {!! Form::number('monto', null, ['class' => 'form-control monto']) !!}
 </div>
 
 <!-- Fecha Field -->
 <div class="form-group col-sm-6">
     {!! Form::label('fecha', 'Fecha:') !!}
-    {!! Form::date('fecha', null, ['class' => 'form-control','id'=>'fecha']) !!}
+    {!! Form::date('fecha', null, ['class' => 'form-control fecha','id'=>'fecha']) !!}
 </div>
 
 <!-- Fecha Field -->
 <div class="form-group col-sm-6">
     {!! Form::label('archivo', 'Archivo:') !!}
-    {!! Form::file('archivo', null, ['class' => 'form-control','id'=>'archivo']) !!}
+    {!! Form::file('archivo', null, ['class' => 'form-control archivo','id'=>'archivo']) !!}
 </div>
 <div class="form-group col-sm-6">
     {!! Form::label('Nro Expediente', 'Nro Expediente:') !!}
-    {!! Form::text('id_expediente',  $expediente->numero, ['class' => 'form-control','disabled']) !!}
+    {!! Form::text('id_expediente',  $expediente->numero, ['class' => 'form-control id_expediente','disabled']) !!}
 </div>
 <div class="col-sm-12">
     {!! Form::hidden('id_expediente',  $expediente->id, ['class' => 'form-control','id'=>'id_expediente']) !!}
